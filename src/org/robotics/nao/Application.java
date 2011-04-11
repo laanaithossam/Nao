088f79985fcf4973cd3c5c197c3982f345300140
laanaithossam <laanaithossam@gmail.com>
2011-04-11 22:49:34 +0200

    Speech Recognition v0.2

--- src/org/robotics/nao/Application.java
+++ src/org/robotics/nao/Application.java
@@ -1,15 +1,20 @@
 package org.robotics.nao;
 
+import java.util.ArrayList;
+
 import org.robotics.nao.view.SpeechRecognition;
 
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.speech.RecognizerIntent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.SubMenu;
+import android.widget.Toast;
 
 public class Application extends Activity {
+	private static final int VOICE_RECOGNITION = 1;
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,22 +23,41 @@ public class Application extends Activity {
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-    	SubMenu m = menu.addSubMenu(0, 1, 0, "Speak");
+    	SubMenu m = menu.addSubMenu(0, VOICE_RECOGNITION, 0, "Speak");
     	m.setIcon(android.R.drawable.ic_btn_speak_now);
     	return super.onCreateOptionsMenu(menu);
     }
     @Override
     //Onclick MenuItem
     public boolean onOptionsItemSelected(MenuItem item) {
-    	Intent intent;
     	switch (item.getItemId()) {
-		case 1:
-			intent=new Intent(Application.this,SpeechRecognition.class);
-	        startActivity(intent);
+		case VOICE_RECOGNITION:
+			startSpeechRecognitionActivity();
 			break;
 		default:
 			break;
 		}
        	return super.onContextItemSelected(item);
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+    	switch (requestCode) {
+		case VOICE_RECOGNITION:
+			if (resultCode == Activity.RESULT_OK)
+				extractSpeechRecognitionResults(data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS));
+			else
+				Toast.makeText(getApplicationContext(),"Erreur", Toast.LENGTH_LONG).show();
+			break;
+		default:
+			break;
+		}
+    }
+    private void startSpeechRecognitionActivity(){
+    	Intent intent=new Intent(Application.this,SpeechRecognition.class);
+        startActivityForResult(intent, VOICE_RECOGNITION);
+    }
+    private void extractSpeechRecognitionResults(ArrayList<String>results){
+    	Toast.makeText(getApplicationContext(),results.get(0), Toast.LENGTH_LONG).show();
     }
 }
--- src/org/robotics/nao/view/SpeechRecognition.java
+++ src/org/robotics/nao/view/SpeechRecognition.java
@@ -3,8 +3,8 @@
  */
 package org.robotics.nao.view;
 
-import java.util.ArrayList;
 import java.util.Locale;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
@@ -15,14 +15,7 @@ import android.speech.RecognizerIntent;
  * Class for voice recognition
  */
 public class SpeechRecognition extends Activity {
-	private static final int VOICE_RECOGNITION_REQUEST_CODE = 1234;
-	Intent intent;
-	ArrayList<String> results;
-	
-	public ArrayList<String> getResults() {
-		return results;
-	}
-	
+	private static final int VOICE_RECOGNITION_REQUEST_CODE = 1;	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
@@ -30,23 +23,21 @@ public class SpeechRecognition extends Activity {
 		startSpeechRecognitionActivity();
 	}
 	private void startSpeechRecognitionActivity(){
-		
-		results=new ArrayList<String>();
-		intent =new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+		Intent intent =new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
 		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, 
 						RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
 		intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "");
 		intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.FRANCE);
 		startActivityForResult(intent, VOICE_RECOGNITION_REQUEST_CODE);
-		
 	}
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-		if (requestCode==VOICE_RECOGNITION_REQUEST_CODE  && resultCode==RESULT_OK){
-			results=data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
-		}
 		super.onActivityResult(requestCode, resultCode, data);
+		if (requestCode==VOICE_RECOGNITION_REQUEST_CODE  && resultCode==RESULT_OK){
+			setResult(resultCode, data);
+			this.finish();
+		}
 	}
 	
 	
