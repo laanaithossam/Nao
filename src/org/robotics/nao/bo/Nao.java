814eeb8269e7d2232a32b5c11a815d6fd500975e
Julien Massot <massot.julien@gmail.com>
2011-07-05 00:28:17 +0200

    Walkto and Say: aren't dummy fucntion anymore

--- src/org/robotics/nao/bo/Nao.java
+++ src/org/robotics/nao/bo/Nao.java
@@ -26,10 +26,16 @@ public class Nao {
 	}
 
 	public void walkTo(float x,float y,float theta){
+		ALProxy motion_proxy = connectionmanager.getProxy("ALMotion");
+
+		connectionmanager.connexion_postCall(motion_proxy, "walkTo", x,y,theta);
+
 		System.out.println("walkTo : X = " + x + " | Y = "+ y +" | O = " + theta);
 	}
 
 	public void say(String message){
+		ALProxy tts_proxy = connectionmanager.getProxy("ALTextToSpeech");
+		connectionmanager.connexion_postCall(tts_proxy, "say", message);
 		System.out.println("say = " + message);
 	}
 
